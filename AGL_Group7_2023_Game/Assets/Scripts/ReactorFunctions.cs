fcf0c4facfabf3706084b19e187dbb716dc8f99e
LegendGman <garrett.guisinger@gmail.com>
2023-10-26 22:13:44 -0700

    Added SFX

--- AGL_Group7_2023_Game/Assets/Scripts/ReactorFunctions.cs
+++ AGL_Group7_2023_Game/Assets/Scripts/ReactorFunctions.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class ReactorFunctions : MonoBehaviour
 {
-    AudioSource alarm;
+    AudioSource audio;
+    public AudioClip alarm;
+    public AudioClip clock;
+    public AudioClip powerup;
     public Camera cam;
     public MeshRenderer imposter;
     public double timer = 0;
@@ -18,8 +21,9 @@ public class ReactorFunctions : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        alarm  = GetComponent<AudioSource>();
+        audio = GetComponent<AudioSource>();
         randVal = Random.Range(35,50);
+        imposter.enabled = false;
     }
 
     // Update is called once per frame
@@ -28,21 +32,19 @@ public class ReactorFunctions : MonoBehaviour
         
         timer += Time.deltaTime;
 
-        if(alarm.isPlaying) {
-            imposter.enabled = true;
-        }
-        else {
-            imposter.enabled = false;
-        } 
-
         if (timer > randVal && !state2) {
-            alarm.Play(0);  
+            imposter.enabled = true;
+            audio.PlayOneShot(alarm);  
             codeInt = Random.Range(1000, 9999);
             codeStr = codeInt.ToString();
             code.text = $"Critical:\n{codeStr}";
             timer = 0;
             randVal = 30;
             state2 = true;
+        }
+
+        if (timer > 5 && state2) {
+            imposter.enabled = false;
         }
 
         if (timer > randVal && state2) {
@@ -53,41 +55,50 @@ public class ReactorFunctions : MonoBehaviour
             
             if (Input.GetKeyDown(KeyCode.Alpha0)) {
                 userInput += "0";
+                audio.PlayOneShot(clock, 0.2f);
             }
             else if (Input.GetKeyDown(KeyCode.Alpha1)) {
                 userInput += "1";
+                audio.PlayOneShot(clock, 0.2f);
             }
             else if (Input.GetKeyDown(KeyCode.Alpha2)) {
                 userInput += "2";
+                audio.PlayOneShot(clock, 0.2f);
             }
             else if (Input.GetKeyDown(KeyCode.Alpha3)) {
                 userInput += "3";
+                audio.PlayOneShot(clock, 0.2f);
             }
             else if (Input.GetKeyDown(KeyCode.Alpha4)) {
                 userInput += "4";
+                audio.PlayOneShot(clock, 0.2f);
             }
             else if (Input.GetKeyDown(KeyCode.Alpha5)) {
                 userInput += "5";
+                audio.PlayOneShot(clock, 0.2f);
             }
             else if (Input.GetKeyDown(KeyCode.Alpha6)) {
                 userInput += "6";
+                audio.PlayOneShot(clock, 0.2f);
             }
             else  if (Input.GetKeyDown(KeyCode.Alpha7)) {
                 userInput += "7";
+                audio.PlayOneShot(clock, 0.2f);
             }
             else if (Input.GetKeyDown(KeyCode.Alpha8)) {
                 userInput += "8";
+                audio.PlayOneShot(clock, 0.2f);
             }
             else if (Input.GetKeyDown(KeyCode.Alpha9)) {
                 userInput += "9";
+                audio.PlayOneShot(clock, 0.2f);
             }
-
-
 
             if ((userInput.Length == 4) && (userInput == codeStr)) {
                state2 = false; 
                code.text = "Safe";
                randVal = Random.Range(35,50);
+               audio.PlayOneShot(powerup, 0.3f);
                timer = 0;
             }
             else if (userInput.Length == 4 && userInput != codeStr) {
